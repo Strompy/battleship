c07d33aee357a050fa2f7b2fa1dccf061d3d830e
James Belta <james.belta@gmail.com>
2020-05-12 14:33:40 -0600

    Build attributes test

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -9,4 +9,11 @@ class CellTest < Minitest::Test
 
     assert_instance_of Cell, cell
   end
+
+  def test_it_has_attributes
+    cell = Cell.new("B4")
+
+    assert_equal "B4", cell.coordinate
+    assert_equal nil, cell.ship
+  end
 end
