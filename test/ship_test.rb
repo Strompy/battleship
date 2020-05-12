80a952953565489ac6049470e5bec910f61f87f0
Phillip Strom <phillip.strom@gmail.com>
2020-05-12 11:54:22 -0600

    Build hit test

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -22,4 +22,20 @@ class ShipTest < Minitest::Test
 
     assert_equal false, cruiser.sunk?
   end
+
+  def test_hit
+    cruiser = Ship.new("Cruiser", 3)
+    cruiser.hit
+
+    assert_equal 2, cruiser.health
+
+    cruiser.hit
+
+    assert_equal 1, cruiser.health
+
+    cruiser.hit
+
+    assert_equal 0, cruiser.health
+    assert_equal true, cruiser.sunk?
+  end
 end
