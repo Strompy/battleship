ee55a8ea264abdd016133990909394baf5eb9d31
Phillip Strom <phillip.strom@gmail.com>
2020-05-12 17:11:35 -0600

    Added assertions to fired_upon test

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -37,9 +37,10 @@ class CellTest < Minitest::Test
     cruiser = Ship.new("Cruiser", 3)
     cell.place_ship(cruiser)
 
+    assert_equal false, cell.fired_upon?
     cell.fire_upon
-    require "pry"; binding.pry
-    p cell.ship.health
-    # assert_equal false, cell.fired_upon?
+
+    assert_equal 2, cell.ship.health
+    assert_equal true, cell.fired_upon?
   end
 end
