017b7c4f22fe98b3b3da5aed90e662ca93349b06
James Belta <james.belta@gmail.com>
2020-05-13 17:22:25 -0600

    Pulled board class test branch

---
+++ lib/cell.rb
@@ -0,0 +1,52 @@
+require './lib/ship'
+
+class Cell
+  attr_reader :coordinate,
+              :ship
+
+  def initialize(coordinate)
+    @coordinate = coordinate
+    @ship = nil
+    @fired_upon = false
+  end
+
+  def empty?
+    @ship == nil
+  end
+
+  def place_ship(ship)
+    @ship = ship
+  end
+
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+    @fired_upon = true
+    if @ship != nil
+      @ship.hit
+    end
+  end
+
+  def render(visible = false)
+
+      if !empty? && fired_upon? == true && @ship.sunk?
+        "X"
+      elsif @ship != nil && fired_upon? == true
+        "H"
+      elsif @ship == nil && fired_upon? == true
+        "M"
+      elsif visible == true && !empty? && fired_upon? == true && @ship.sunk?
+        "X"
+      elsif visible == true && @ship != nil && fired_upon? == true
+        "H"
+      elsif visible == true && @ship == nil && fired_upon? == true
+        "M"
+      elsif visible == true && @ship != nil
+        "S"
+      else
+        "."
+      end
+  end
+end
