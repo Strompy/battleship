25c8174c21238d0229e79575cb99e980feb09200
Phillip Strom <phillip.strom@gmail.com>
2020-05-14 15:37:53 -0600

    Add true valid placement assertions

--- test/board_test.rb
+++ test/board_test.rb
@@ -41,6 +41,8 @@ class BoardTest < Minitest::Test
     assert_equal false, board.valid_placement?(submarine, ["C1", "B1"])
     assert_equal false, board.valid_placement?(cruiser, ["A1", "B2", "C3"])
     assert_equal false, board.valid_placement?(submarine, ["C2", "D3"])
+    assert_equal true, board.valid_placement?(submarine, ["A1", "A2"])
+    assert_equal true, board.valid_placement?(cruiser, ["B1", "C1", "D1"])
   end
 
   def test_coordinates_in_same_row?
