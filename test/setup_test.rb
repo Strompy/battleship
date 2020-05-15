457072f064c28a0565ad30bcda864f882226f450
James Belta <james.belta@gmail.com>
2020-05-15 17:51:37 -0600

    Build a setup

--- test/setup_test.rb
+++ test/setup_test.rb
@@ -6,10 +6,15 @@ require './lib/cell'
 require './lib/setup'
 
 class SetupTest < Minitest::Test
+  def setup
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+    @player = Board.new
+    @computer = Board.new
+    @setup = Setup.new(@player, @computer)
+  end
+
   def test_it_exists
-    player = Board.new
-    computer = Board.new
-    setup = Setup.new(player, computer)
-    assert_instance_of Setup, setup
+    assert_instance_of Setup, @setup
   end
 end
