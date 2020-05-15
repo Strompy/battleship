79719287e5e087b7b62a0800765e0537f1af5834
James Belta <james.belta@gmail.com>
2020-05-15 17:58:41 -0600

    Edit setup and test to match instance variables

--- test/setup_test.rb
+++ test/setup_test.rb
@@ -9,12 +9,17 @@ class SetupTest < Minitest::Test
   def setup
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
-    @player = Board.new
-    @computer = Board.new
-    @setup = Setup.new(@player, @computer)
+    @player1 = Board.new
+    @player2 = Board.new
+    @setup = Setup.new(@player1, @player2)
   end
 
   def test_it_exists
     assert_instance_of Setup, @setup
+  end
+
+  def test_it_has_attributes
+    assert_equal @player1, @setup.player
+    assert_equal @player2, @setup.computer
   end
 end
